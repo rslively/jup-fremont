6f26996c850c0904ecbfd88b5d7259a6d5c99fda
Rick Lively <rslively2003@yahoo.com>
2018-08-19 15:34:17 -0600

    fixes

--- fremontworkflow/data.py
+++ fremontworkflow/data.py
@@ -3,9 +3,13 @@ from urllib.request import urlretrieve
 import pandas as pd
 import matplotlib.pyplot as plt
 
-URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 
-def get_fremont_data(filename='Fremont.csv', url=URL, force_download=False):
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL,
+        force_download=False):
+    """
+    Download and cache Seattle Fremont Bridge bicycle crossing data
+    """
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
     data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
